bb75ab4d31a3deb4b337767d276d0857c40b0772
Lucas VASSEUR <lvasseur@e2r9p10.42.fr>
2017-03-06 13:11:05 +0100

    init

--- includes/rt.h
+++ includes/rt.h
@@ -6,13 +6,18 @@
 /*   By: lvasseur <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/06 12:34:20 by lvasseur          #+#    #+#             */
-/*   Updated: 2017/03/06 12:36:14 by lvasseur         ###   ########.fr       */
+/*   Updated: 2017/03/06 13:10:54 by lvasseur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RT_H
 
 # define RT_H
-# define A 1
+
+# include "../libft/libft.h"
+# include <math.h>
+# include <mlx.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 #endif
