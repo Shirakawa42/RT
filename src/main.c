a50d21066bf52b9e012e32de37a3df08c2c8d176
Lorenzo MERESSE <lomeress@e1r10p17.42.fr>
2017-03-10 16:40:23 +0100

    debut1

--- src/main.c
+++ src/main.c
@@ -6,19 +6,26 @@
 /*   By: lvasseur <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/06 12:35:31 by lvasseur          #+#    #+#             */
-/*   Updated: 2017/03/10 16:24:58 by lomeress         ###   ########.fr       */
+/*   Updated: 2017/03/10 16:39:34 by lomeress         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/rt.h"
 
-int		my_key_funct(int kc, void *param)
+int		my_key_funct(int kc, t_mlx *st)
 {
-	t_mlx *st;
-
-	st =  (t_mlx*)param;
 	if (kc == 53)
+	{
+		free(st);
 		exit(1);
+	}
+	return (0);
+}
+
+int		my_key_funct2(t_mlx *st)
+{
+	free(st);
+	exit(1);
 	return (0);
 }
 
@@ -31,7 +38,8 @@ int		main(void)
 	st->win = mlx_new_window(st->mlx, W, H, "RT");
 	st->img = mlx_new_image(st->mlx, W, H);
 	st->gda = mlx_get_data_addr(st->img, &st->bpx, &st->size_line, &st->end);
-	mlx_key_hook(st->win, my_key_funct, &st);
+	mlx_key_hook(st->win, my_key_funct, st);
+	mlx_hook(st->win, 17, 1L << 6, my_key_funct2, st);
 	mlx_loop(st->mlx);
 	return (0);
 }
