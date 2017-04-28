99acbb232afd4f0cefbf95ebfec3cc98cb0e7897
momolapeche <4n4tol3@gmail.com>
2017-04-28 10:31:01 +0200

    Add files via upload

--- includes/rt.h
+++ includes/rt.h
@@ -6,7 +6,7 @@
 /*   By: lvasseur <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/06 12:34:20 by lvasseur          #+#    #+#             */
-/*   Updated: 2017/04/11 18:33:17 by rmenegau         ###   ########.fr       */
+/*   Updated: 2017/04/13 17:55:53 by lvasseur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 # define RT_H
 
 #include <math.h>
-#include "SDL.h"
-#define H 720
-#define W 720
+#include "./include/SDL2/SDL.h"
+#define H 520
+#define W 520
 
 typedef struct	s_vec
 {
@@ -34,12 +34,14 @@ typedef struct	s_ray
 
 typedef struct	s_color
 {
-	int	r;
-	int	g;
-	int	b;
+	double	r;
+	double	g;
+	double	b;
 }				t_color;
 
 #define SPHERE 1
+#define PLANE 2
+#define CYLINDER 3
 
 typedef struct	s_sphere
 {
@@ -47,9 +49,24 @@ typedef struct	s_sphere
 	double	r;
 }				t_sphere;
 
+typedef struct	s_plane
+{
+	t_vec	n;
+	t_vec	p;
+}				t_plane;
+
+typedef struct	s_cylinder
+{
+	t_vec	p;
+	double	r;
+}				t_cylinder;
+
 union	u_shape
 {
 	t_sphere	sphere;
+	t_plane		plane;
+	t_cylinder	cylinder;
+	int			texture;
 };
 
 typedef struct	s_object
@@ -76,27 +93,52 @@ typedef struct	s_light
 {
 	int				type;
 	t_color			color;
+	double			intensity;
 	union u_light	light;
 }				t_light;
+
+typedef struct s_scene
+{
+	t_ray		camera;
+	t_light		*lights;
+	t_object	*objects;
+}				t_scene;
+
+typedef struct	s_env
+{
+	t_scene	scene;
+	int		editmod;
+}				t_env;
 
 // vector.c
 double		dot(t_vec a, t_vec b);
 t_vec		create_vec(double x, double y, double z);
-void	normalize(t_vec *v);
+t_vec		get_point(t_ray ray, double t);
+t_vec		bisector(t_vec v, t_vec l);
+void		normalize(t_vec *v);
 
 // create_lights.c
-t_light		create_light_bulb(double x, double y, double z, t_color color);
+t_light		create_light_bulb(double x, double y, double z, t_color color, double intensity);
 
 // create_objects.c
-t_object	create_sphere(double x, double y, double z, double r, t_color color, double reflection);
+t_object	create_sphere(double x, double y, double z, double r, t_color color, double reflection, int texture);
+t_object	create_plane(t_vec p, t_vec n, t_color color, double reflection, int texture);
+t_object	create_cylinder(t_vec p, double r, t_color color, double reflection, int texture);
 
 // color.c
-t_color		create_color(int r, int g, int b);
+t_color		create_color(double r, double g, double b);
 
 // intersects.c
 int			sphere_intersect(union u_shape shape, t_ray ray, double *t);
+int			plane_intersect(union u_shape shape, t_ray ray, double *t);
+int			cylinder_intersect(union u_shape shape, t_ray ray, double *t);
 
 // normals.c
 t_vec	sphere_normal(union u_shape shape, t_vec p);
+t_vec	plane_normal(union u_shape shape, t_vec p);
+t_vec	cylinder_normal(union u_shape shape, t_vec p);
+
+// textures.c
+t_vec	text1(t_vec n, int text);
 
 #endif
