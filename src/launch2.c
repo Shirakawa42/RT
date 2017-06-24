d5105aec18b0cc5e45a80dccec0b64b0a8ad5af8
Lorenzo MERESSE <lomeress@e1r5p9.42.fr>
2017-06-24 13:22:13 +0200

    voil

--- src/launch2.c
+++ src/launch2.c
@@ -6,7 +6,7 @@
 /*   By: lomeress <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/23 15:37:29 by lomeress          #+#    #+#             */
-/*   Updated: 2017/06/23 17:47:52 by lomeress         ###   ########.fr       */
+/*   Updated: 2017/06/24 13:19:23 by lomeress         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,73 +22,74 @@ void	app(t_env *e)
 		e->colorsave[0] = sepia(e->colorsave[0]);
 }
 
+void	init_launch(void *truc, t_ray *ray, t_env *e, t_npt *np)
+{
+	np->y = 0;
+	np->w = e->editmod ? W / 3 : W;
+	np->h = e->editmod ? H / 3 : H;
+	np->wsave = np->w;
+	np->hsave = np->h;
+	if (np->number < NB_THREADS / 2)
+		np->h /= 2;
+	else
+		np->y = np->h / 2;
+	if (np->number < NB_THREADS / 2)
+		np->savex = (np->number * (np->w / (NB_THREADS / 2)));
+}
+
+void	launch_n(void *truc, t_ray *ray, t_env *e, t_npt *np)
+{
+	while (np->x < np->w)
+	{
+		np->n = 0;
+		while (np->n < np->ssaa_squared)
+		{
+			ray->d = vec((((double)np->x + (double)(np->n % e->ssaa) /
+							(double)(e->ssaa)) / np->wsave - 0.5),
+				(0.5 - ((double)np->y + (double)(np->n / e->ssaa) /
+						(double)(e->ssaa)) / np->hsave), 1);
+			ray->gogol.x = e->scene.rotation.tmp1;
+			ray->gogol.y = e->scene.rotation.tmp2;
+			ray->gogol.z = e->scene.rotation.tmp3;
+			ray->d = matrice2(ray->d, ray->gogol);
+			normalize(&ray->d);
+			if (e->editmod != 0)
+				e->index = 0;
+			else
+				e->index = NB_REFLEC;
+			e->colorsave[np->n] = is_color_good(ray_trace(*ray, *e));
+			np->n++;
+		}
+		apply_color((t_void*)truc, *e, np->x, np->y);
+		np->x++;
+	}
+	np->y++;
+}
+
 void	*launch(void *truc)
 {
 	t_ray			ray;
-	int				y;
-	int				x;
-	double			w;
-	double			h;
 	t_env			e;
-	int				number;
-	int				savex;
-	int				wsave;
-	int				hsave;
-	int				n;
-	int				ssaa_squared;
+	t_npt			np;
 
 	pthread_mutex_lock(&(*(t_void*)truc).mutex);
 	e = (*(t_void*)truc).e;
-	number = (*(t_void*)truc).number++;
+	np.number = (*(t_void*)truc).number++;
 	pthread_mutex_unlock(&(*(t_void*)truc).mutex);
-	y = 0;
-	w = e.editmod ? W / 3 : W;
-	h = e.editmod ? H / 3 : H;
-	wsave = w;
-	hsave = h;
-	if (number < NB_THREADS / 2)
-		h /= 2;
-	else
-		y = h / 2;
-	if (number < NB_THREADS / 2)
-		savex = (number * (w / (NB_THREADS / 2)));
-	else
-		savex = ((number - (NB_THREADS / 2)) * (w / (NB_THREADS / 2)));
-	w = savex + (w / (NB_THREADS / 2));
+	init_launch(truc, &ray, &e, &np);
+	if (!(np.number < NB_THREADS / 2))
+		np.savex = ((np.number - (NB_THREADS / 2)) * (np.w / (NB_THREADS / 2)));
+	np.w = np.savex + (np.w / (NB_THREADS / 2));
 	ray.o = e.scene.camera.o;
 	radian(&e.scene.rotation.tmp1, &e.scene.rotation.tmp2,
 			&e.scene.rotation.tmp3, e);
-	ssaa_squared = e.ssaa * e.ssaa;
+	np.ssaa_squared = e.ssaa * e.ssaa;
 	if (e.editmod >= 1)
-		ssaa_squared = 1;
-	while (y < h)
+		np.ssaa_squared = 1;
+	while (np.y < np.h)
 	{
-		x = savex;
-		while (x < w)
-		{
-			n = 0;
-			while (n < ssaa_squared)
-			{
-				ray.d = vec((((double)x + (double)(n % e.ssaa) /
-								(double)(e.ssaa)) / wsave - 0.5),
-					(0.5 - ((double)y + (double)(n / e.ssaa) /
-							(double)(e.ssaa)) / hsave), 1);
-				ray.gogol.x = e.scene.rotation.tmp1;
-				ray.gogol.y = e.scene.rotation.tmp2;
-				ray.gogol.z = e.scene.rotation.tmp3;
-				ray.d = matrice2(ray.d, ray.gogol);
-				normalize(&ray.d);
-				if (e.editmod != 0)
-					e.index = 0;
-				else
-					e.index = NB_REFLEC;
-				e.colorsave[n] = is_color_good(ray_trace(ray, e));
-				n++;
-			}
-			apply_color((t_void*)truc, e, x, y);
-			x++;
-		}
-		y++;
+		np.x = np.savex;
+		launch_n(truc, &ray, &e, &np);
 	}
 	return (0);
 }
